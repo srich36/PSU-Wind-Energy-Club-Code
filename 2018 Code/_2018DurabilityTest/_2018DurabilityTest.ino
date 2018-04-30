478c745d3c1d45fabf5bd8cb32347696fe20ff5d
srich36 <srich366@gmail.com>
2018-04-30 16:41:01 -0400

    Implements untested EEPROM memory and creates untested durability test code

---
+++ 2018 Code/_2018DurabilityTest/_2018DurabilityTest.ino
@@ -0,0 +1,100 @@
+#include <Servo.h>
+
+const int PWM_PIN = 3; 
+const int SERVO_PITCH_PIN = 9; 
+const int LOWER_VOLTAGE_LIMIT = 10;
+const int UPPER_VOLTAGE_LIMIT = 15;
+const int MAINTAIN_VOLTAGE_IN = 13;
+const int DUTY_CYCLE_RATIO = 5;
+static int currentPitch;
+
+
+double turbineVoltageBefore;
+const int STARTUP_PITCH = 45; //Need to verify pitch for new turbine -> should be verified now
+const double VOLTAGE_DIVIDER_TURBINE = 13.015; //This should be the same as last year so we are good
+
+
+void determinePitch(double turbineVoltage);
+int calculateDutyCycle();
+
+Servo pitch;
+
+void setup(){
+  pinMode(PWM_PIN, OUTPUT);
+  turbineVoltageBefore = -1;
+  pitch.attach(SERVO_PITCH_PIN);
+  pitch.write(STARTUP_PITCH);
+  currentPitch = STARTUP_PITCH;
+}
+
+void loop(){
+  double turbineVoltage;
+  int dutyCycle;
+  turbineVoltage = VOLTAGE_DIVIDER_TURBINE*((double)analogRead(A0))*5.0/1023.0;
+  if(turbineVoltageBefore!= -1){ //This is not the first voltage reading.
+    determinePitch(turbineVoltage);
+  }
+  //Else do nothing because it's only the first loop
+  //Now calculate the duty cycle
+  
+  
+  dutyCycle = calculateDutyCycle(turbineVoltage);
+  analogWrite(PWM_PIN, dutyCycle);
+  
+  //Move to the t+1 loop domain
+  turbineVoltageBefore = turbineVoltage;
+}
+
+void determinePitch(double turbineVoltage){
+  
+  //Case 1:
+  if(turbineVoltage < LOWER_VOLTAGE_LIMIT){
+    pitch.write(currentPitch+1);
+    currentPitch++;
+  }
+  else if(turbineVoltage> LOWER_VOLTAGE_LIMIT && turbineVoltage < MAINTAIN_VOLTAGE_IN){ //Case 2
+    if(turbineVoltage>=turbineVoltageBefore){
+      //Do nothing
+    }
+    else{
+      pitch.write(currentPitch+1);
+      currentPitch++;
+    }
+    
+  }
+  else if(turbineVoltage >= MAINTAIN_VOLTAGE_IN && turbineVoltage < UPPER_VOLTAGE_LIMIT){  //Case 3
+    
+    if(turbineVoltage>=turbineVoltageBefore){
+      pitch.write(currentPitch-1);
+      currentPitch--;
+    }
+    else{
+      //Do nothing
+    }
+    
+  }
+  else if(turbineVoltage>=UPPER_VOLTAGE_LIMIT){
+    pitch.write(currentPitch - 3);
+    currentPitch -= 3;
+  }
+  
+  else{
+    Serial.println("Edge case we have not accounted for. ");
+    Serial.print("Turbine Voltage is: ");
+    Serial.println(turbineVoltage);
+    Serial.print("And voltage before is: ");
+    Serial.println(turbineVoltageBefore);
+  }
+  
+}
+
+int calculateDutyCycle(double turbineVoltage){
+  if(turbineVoltage > 5){
+    return int((255*DUTY_CYCLE_RATIO)/turbineVoltage);
+  }
+  else{
+    return 255; //Max duty cycle 
+  }
+  
+  
+}
--- 2018 Code/_2018controlcodev2/_2018controlcodev2.ino
+++ 2018 Code/_2018controlcodev2/_2018controlcodev2.ino
@@ -1,4 +1,13 @@
+//TODO: 
+//     1. Test EEPROM memory. 
+//     2. Figure out how to write the infer wind speed function
+//     3. Confirm pin constants
+//     4. Figure out more logic to control how pitch works after 10m/s
+
+
+
 #include <Servo.h>
+#include <EEPROM.h>
 
 //All pins are by default input so you don't have to specify input,and you do not on analog pins.
 
@@ -15,14 +24,18 @@
 //****************************************************//
 //
 //1. Is resistance constant? If not, which resistance should I use? Because it is always changing in the equation and optimal power outputs. 
+//A: the effective resistance is changing through the duty cycle. The r in the equations is that constant 50
 //
 //2. How are we determining wind speed? Using the data that has a given Vin and given power output at
+//A: Gven inputs of voltage, RPM, and power, we can estimate the wind speed 
 //
 //3. On the resistance power curve data plot, are those optimal pitch angles at the given wind speed all at a 50ohm resistance? Is that what we are operating at constantly?
+//A: We are just doing buckets of pitch angles for a given wind speed
 //
 //4. So we want to fix power, do we need to find the RPM at the optimal pitch angle at 11 m/s and calculate that power
 // Then keep that is constant in the duty cycle equation? Because if so, then we need to test more at 11m/s because we don't have
 // data there at a 50 ohm resistance.
+//A: Yes the power at 11m/s is a constant we will maintain.
 
 
 
@@ -38,14 +51,17 @@
 //****************************************************//
 //******************Arduino Pin constants*************//
 //****************************************************//
-const int pwmPin = 3;               //Digital
-const int powerToGateDrivePin = 8;  //Digital
-const int servoPitchPin = 9;        //Digital
-const int killSwitchPin = 1;        //Digital
-const int turbineVoltagePin = 0;    //Analog
-const int loadVoltagePin = 5; // This value is to be changed when we make the circuit
-
-Servo pitch;
+const int PWM_PIN = 3;               //Digital
+const int LOAD_ARDUINO_PIN = 8;      //Digital 
+const int SERVO_PITCH_PIN = 9;        //Digital
+const int KILL_SWITCH_PIN = 1;        //Digital
+const int TURBINE_vOLTAGE_PIN = 0;    //Analog
+const int PWM_CONVERSION = 255;       //the arduino operates on a 0-255 scale for pwm so the duty cycle needs to be within this range
+const int FIVE_TO_SEVEN_PITCH_ANGLE = 61;
+const int SEVEN_TO_TEN_PITCH_ANGLE = 57;
+const int TEN_PLUS_INITIAL_PITCH_ANGLE = 48;
+static int currentPitch;
+static boolean breakedInCompetition = false;
 
 //****************************************************//
 //****************Control System Constants************//
@@ -57,34 +73,37 @@ const double VOLTAGE_DIVIDER_TURBINE = 13.015; //This should be the same as last
 const double VOLTAGE_DIVIDER_LOAD = 11.13;//This needs to be calculated for our new load
 const double MAX_VOLTAGE = 45;
 const int RESISTANCE = 50; //Is this a constant or does it change? 
+const double POWER_AT_11MS = 45.5;
 
-
+Servo pitch;
 
 //****************************************************//
 //****************Function Prototypes*****************//
 //****************************************************//
 
-double inferWindSpeed(double voltageIn);
-boolean determinePCCDisconnect(double turbineVoltage, double loadVoltage);
-int determineOptimumPitch(double);
-void processDisconnectedState();
-void processKillState(boolean killSwitch);
+double inferWindSpeed(double voltageIn, double RPM, double Power);
+//int determineOptimumPitch(double);
+//void processDisconnectedState();
 int calculateDutyCycle11(double resistance, double power, double turbineVoltage);
 double getRPMfromVoltageIn(double VoltageIn);
 double calculatePowerFromRPM(double RPM);
-
+void pitchToPitchAngleBucket(double windSpeed);
 
 void setup(){
   Serial.begin(9600);
-  pinMode(pwmPin, OUTPUT);                 //Configures PWM pin as output
+  pinMode(PWM_PIN, OUTPUT);  //Configures PWM pin as output
+  pinMode(LOAD_ARDUINO_PIN, INPUT);
   //pinMode(servoPitch, OUTPUT);  Don't think I need this //configures servo pin as output
-  //pinMode(loadVoltagePin, INPUT);      //Might need to put A's in front of these because they are analog pins
   //pinMode(A0, INPUT);   //But don't think so because I'm calling analog read on them
-  //pinMode(killSwitchPin, INPUT);
-  pitch.attach(servoPitchPin);
-  digitalWrite(powerToGateDrivePin, HIGH);
+  pitch.attach(SERVO_PITCH_PIN);
+  //digitalWrite(powerToGateDrivePin, HIGH);             //Not sure if we need this because the gate drive might not be working
   Serial.println("Beginning the control system code");
-  pitch.write(STARTUP_PITCH);
+  if(EEPROM.read(0)){
+    pitch.write(EEPROM.read(0+1));
+  }
+  else{
+    pitch.write(STARTUP_PITCH);
+  }
 }
 
 void loop(){
@@ -94,63 +113,57 @@ void loop(){
   //****************************************************//
 
   double turbineVoltage;
-  double loadVoltage;
   double inferredWindSpeed;
-  boolean killSwitch;
-  boolean disconnected;
-  
+  boolean breakNeeded;
   double RPM;
+  double power;
   int dutyCycle;
-  static boolean disconnectedState = false;
-  static boolean killState = false;
 
+  if(digitalRead(LOAD_ARDUINO_PIN) == HIGH){
+    breakNeeded = true;
+    processDisconnectedState(breakNeeded);
+  }
+  else
+    breakNeeded = false; 
   turbineVoltage = VOLTAGE_DIVIDER_TURBINE*((double)analogRead(A0))*5.0/1023.0;
   RPM = getRPMfromVoltageIn(turbineVoltage);
-
-  //*****************************************************************//
-  //**********Will read load voltage here when it is done************//
-  //*****************************************************************//
-  
-  
-  killSwitch = (digitalRead(A1) == LOW);
-  disconnectedState = determinePCCDisconnect(turbineVoltage, loadVoltage);
-  processDisconnectedState(disconnectedState);                                  //May need to make sure it doesn't pitch away from brake pitch immediately
-  processKillState(killSwitch);
-  inferredWindSpeed = inferWindSpeed(turbineVoltage);
+  power = calculatePowerFromRPM(RPM);                           
+  inferredWindSpeed = inferWindSpeed(turbineVoltage, RPM, power);
   if(inferredWindSpeed < 11){
     dutyCycle = calculateDutyCycle(RESISTANCE, calculatePowerFromRPM(RPM), turbineVoltage);
-    analogWrite(pwmPin, dutyCycle);
+    analogWrite(PWM_PIN, dutyCycle);
   }
   else{
-    dutyCycle = -1; //Placeholder to find out duty cycle given constant power. Depends on answers to the above questions
+    dutyCycle = calculateDutyCycle(RESISTANCE, POWER_AT_11MS, turbineVoltage); //Placeholder to find out duty cycle given constant power. Depends on answers to the above questions
+    analogWrite(PWM_PIN, dutyCycle);
   }
 
 
 }
 
 void processDisconnectedState(boolean disconnected){
-  if(disconnected)
+  if(disconnected){
+    breakedInCompetition = true;
+    EEPROM.write(0,breakedInCompetition);
+    EEPROM.write(0+1,currentPitch);
     pitch.write(BRAKE_PITCH);
-  delay(1000);
+  }
+  else{
+    breakedInCompetition = false;
+    EEPROM.write(0,breakedInCompetition);
+  }
+  delay(10000);
   return;
 }
 
-void processKillState(boolean killSwitchState){
-  if(killSwitchState){
-    Serial.println("Kill switch is hit. Braking the turbine.");
-  }
-}
-
-double inferWindSpeed(double voltageIn){
+double inferWindSpeed(double voltageIn, double RPM, double power){
   return 0;
 }
 
-boolean determinePCCDisconnect(double turbineVoltage, double loadVoltage){
-  return false;
-}
+
 
 double getRPMfromVoltageIn(double turbineVoltage){
-  return 1; //Placeholder for more data that needs to go in here.
+  return 108*turbineVoltage-462; //Placeholder for more data that needs to go in here.
 }
 
 double calculatePowerFromRPM(double RPM){
@@ -158,6 +171,32 @@ double calculatePowerFromRPM(double RPM){
 }
 
 int calculateDutyCycle(double resistance, double power, double turbineVoltage){
-  return int(sqrt(resistance*power)/turbineVoltage);
+  int dutyCycle = PWM_CONVERSION*int(sqrt(resistance*power)/turbineVoltage);
+  if(dutyCycle > 255){
+    Serial.print("Over 255 duty cycle. Something is wrong. Duty cycle is equal to: ");
+    Serial.println(dutyCycle);
+  }
+  else{
+    Serial.print("Sending a duty cycle of: ");
+    Serial.println(dutyCycle);
+  }
+  
+  return dutyCycle;
+}
+
+void pitchToPitchAngleBucket(double windSpeed){
+  if(windSpeed > 5 && windSpeed < 7){
+    pitch.write(FIVE_TO_SEVEN_PITCH_ANGLE);
+    currentPitch = FIVE_TO_SEVEN_PITCH_ANGLE;
+  }
+  else if(windSpeed>=7 && windSpeed < 10){
+    pitch.write(SEVEN_TO_TEN_PITCH_ANGLE);
+    currentPitch = SEVEN_TO_TEN_PITCH_ANGLE;
+  }
+  else if(windSpeed > 10){
+    pitch.write(TEN_PLUS_INITIAL_PITCH_ANGLE);
+    currentPitch = TEN_PLUS_INITIAL_PITCH_ANGLE;
+  }
+
 }
 
